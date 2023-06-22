ef08c22dd225207fee2323bb9ba3e9294c0250fa
FILIPPE CARLOS <80841522+filippecarlos@users.noreply.github.com>
2023-06-21 21:03:09 -0300

    Add files via upload

---
+++ Curso PHP/db/conexao_pdo.php
@@ -0,0 +1,19 @@
+<?php
+
+function novaConexao($banco = 'curso_php')
+{
+    $servidor = '127.0.0.1:3308';
+    $usuario = 'root';
+    $senha = '';
+
+    try {
+        $conexao = new PDO(
+            "mysql:host=$servidor;dbname=$banco",
+            $usuario,
+            $senha
+        );
+        return $conexao;
+    } catch (PDOException $e) {
+        die('Erro: ' . $e->getMessage());
+    }
+}
